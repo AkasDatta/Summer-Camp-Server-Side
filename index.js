30ffa5fafbdc51259dc6764071f159b6975531d4
Akas_Datta <012akasdatta@gmail.com>
2023-06-15 02:12:37 +0600

    added classes apprrove and denied

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+// const stripe = require("stripe")(process.env.PAYMENT_SK);
 const port = process.env.PORT || 5000;
+
 
 // middleware
 app.use(cors());
@@ -17,16 +20,16 @@ const verifyJWT = (req, res, next) => {
   //bearer token
   const token = authorization.split(' ')[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodede) => {
     if(err){
       return res.status(401).send({error: true, message: 'unauthorized access'})
     }
-    req.decoded = decoded;
+    req.decoded = decodede;
     next();
   })
 }
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cv3dsgq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -55,8 +58,19 @@ async function run() {
       res.send({token})
     })
 
+    // warning: use verifyJwt before using verifyAdmin
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await userCollection.findOne(query);
+      if(user?.role !== 'admin'){
+        return res.status(403).send({error: true, message: 'forbidden message'});
+      }
+      next();
+    }
+
     //savedusers related APIs
-    app.get('/savedusers', async(req, res) => {
+    app.get('/savedusers', verifyJWT, verifyAdmin, async(req, res) => {
       const result = await savedusersCollection.find().toArray();
       res.send(result);
     })
@@ -70,6 +84,19 @@ async function run() {
       }
 
       const result = await savedusersCollection.insertOne(saveduser);
+      res.send(result);
+    });
+
+    app.get('/savedusers/admin/:email', verifyJWT, async(req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query = {email: email}
+      const user = await savedusersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'}
       res.send(result);
     })
     
@@ -92,6 +119,39 @@ async function run() {
       const result = await classCollection.find().toArray();
       res.send(result);
     });
+
+    app.post('/classes', verifyJWT, verifyAdmin, async(req, res) => {
+      const classItem = req.body;
+      console.log(classItem)
+      const result = await classCollection.insertOne(classItem)
+      res.send(result);
+    })
+
+    app.put('/classes/approved/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          status: 'approved'
+        },
+      };
+      const result = await classCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
+    app.put('/classes/denied/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          status: 'denied'
+        },
+      };
+      const result = await classCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
 
     // users related APIs
     app.get('/users', async (req, res) => {
